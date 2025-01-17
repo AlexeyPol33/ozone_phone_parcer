52d7e10d0ff2754aaf8f81dc8b11ad32839c87c5
Alexey Poltavskiy <79776177342@yandex.ru>
2025-01-17 23:34:10 +0300

    deletion of duplicate urls

--- smartphone_parser/smartphone_parser/spiders/url_spider.py
+++ smartphone_parser/smartphone_parser/spiders/url_spider.py
@@ -4,11 +4,12 @@ from .base_spider import BaseSpider
 class URLSpider(BaseSpider):
     name = "url_spider"
     file_name = "urls"
-    scroll = 7000
+    scroll = 14000
     start_urls = ["https://www.ozon.ru/category/smartfony-15502/?sorting=rating"]
 
     results = []
 
     def parse(self, response):
         urls = response.css("div.wi1_23").css("a::attr(href)").extract()
-        self.results = ["https://www.ozon.ru" + f"{url}\n" for url in urls][:100]
+        self.results = ["https://www.ozon.ru" + f"{url}\n" for url in urls]
+        self.results = list(set(self.results))[:100]
